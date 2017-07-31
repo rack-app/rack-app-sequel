6daab1b13e3c683dd8d25b2d2476aee5359d6722
Adam Luzsi <adamluzsi@gmail.com>
2017-07-31 11:29:18 +0200

    pass connection block to sequel connect

--- lib/rack/app/sequel.rb
+++ lib/rack/app/sequel.rb
@@ -6,8 +6,8 @@ module Rack::App::SeQueL
   require 'rack/app/sequel/version'
   require 'rack/app/sequel/migration'
 
-  def open_connection
-    Sequel.connect(ENV.fetch('DATABASE_URL'))
+  def open_connection(&block)
+    Sequel.connect(ENV.fetch('DATABASE_URL'), &block)
   end
 
   module_function :open_connection
